4adc8620d867269ccef345a5f76f8ed3d70c2152
Marco Salierno <37928190+hidingbit@users.noreply.github.com>
2023-02-01 12:14:15 +0100

    Updated application themes.

--- arts/lib/ui/styles.dart
+++ arts/lib/ui/styles.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 const Color darkOrange = Color(0xffE68532);
 const Color lightOrange = Color(0xFFEB9E5C);
+const Color darkBlue =  Color(0xff113197);
 
 final ButtonStyle largeButtonStyle = ElevatedButton.styleFrom(
   backgroundColor: const Color(0xFFE68532),
@@ -29,29 +30,171 @@ final ButtonStyle topButtonStyle = ElevatedButton.styleFrom(
     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0))
 );
 
-final ThemeData lightTheme = ThemeData.light().copyWith(
+final ThemeData lightTheme = ThemeData(
+  fontFamily: 'JosefinSans',
   appBarTheme: const AppBarTheme(
-      backgroundColor: Color(0xff113197),
-      actionsIconTheme: IconThemeData(color: lightOrange),
-      iconTheme: IconThemeData(color: lightOrange),
-      titleTextStyle: TextStyle(color: Colors.white, fontSize: 20)
+      centerTitle: true,
+      elevation: 1.0,
+      backgroundColor: Colors.transparent,
+      shadowColor: Color(0x33000000),
+      actionsIconTheme: IconThemeData(color: darkOrange),
+      iconTheme: IconThemeData(color: darkOrange),
+      titleTextStyle: TextStyle(color: darkBlue, fontSize: 18, fontFamily: 'JosefinSans', fontWeight: FontWeight.bold)
   ),
-  iconTheme: const IconThemeData(color: lightOrange),
+  tabBarTheme: const TabBarTheme(
+    labelColor: darkBlue,
+    indicator: UnderlineTabIndicator(borderSide: BorderSide(color: darkBlue, width: 2.0))
+  ),
+  iconTheme: const IconThemeData(color: darkOrange, shadows: [], size: 28),
   dividerColor: Colors.black26,
-  errorColor: Colors.red,
-  colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.green),
+  dialogTheme: DialogTheme(
+    shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30.0)
+    ),
+  ),
+  colorScheme: ColorScheme.fromSeed(
+      brightness: Brightness.light,
+      seedColor: Colors.orange
+  ).copyWith(
+    primary: darkOrange,
+    secondary: darkOrange,
+    tertiary: darkBlue,
+    error: Colors.red
+  ),
+  textTheme: const TextTheme().copyWith(
+    bodyLarge: const TextStyle(color: darkBlue),
+    bodyMedium: const TextStyle(color: darkBlue),
+    titleLarge: const TextStyle(color: darkBlue),
+    titleMedium: const TextStyle(color: darkBlue),
+    titleSmall: const TextStyle(color: Colors.blueGrey),
+  ),
+  radioTheme: RadioThemeData(fillColor: MaterialStateProperty.resolveWith((states) => darkOrange)),
+  inputDecorationTheme: const InputDecorationTheme().copyWith(
+    border: OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
+    contentPadding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+    suffixIconColor: lightOrange
+  ),
+  bottomNavigationBarTheme: const BottomNavigationBarThemeData().copyWith(
+    unselectedItemColor: Colors.blueGrey,
+    unselectedLabelStyle: const TextStyle(color: Colors.blueGrey)
+  ),
 );
 
-final ThemeData darkTheme = ThemeData.dark().copyWith(
+final ThemeData darkTheme = ThemeData(
+  fontFamily: 'JosefinSans',
   appBarTheme: const AppBarTheme(
-    backgroundColor: Color(0xff113197),
-    actionsIconTheme: IconThemeData(color: lightOrange),
-    iconTheme: IconThemeData(color: lightOrange),
-    titleTextStyle: TextStyle(color: Colors.white, fontSize: 20)
+      centerTitle: true,
+      elevation: 1.0,
+      backgroundColor: Colors.transparent,
+      shadowColor: Color(0x33FFFFFF),
+      actionsIconTheme: IconThemeData(color: darkOrange),
+      iconTheme: IconThemeData(color: darkOrange),
+      titleTextStyle: TextStyle(color: Colors.white, fontSize: 18, fontFamily: 'JosefinSans', fontWeight: FontWeight.bold)
   ),
-  backgroundColor: const Color(0xff242F72),
-  iconTheme: const IconThemeData(color: lightOrange),
-  dividerColor: const Color(0xff414c9c),
-  errorColor: Colors.red,
-  colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.green),
+  tabBarTheme: const TabBarTheme(
+      labelColor: Colors.white,
+      indicator: UnderlineTabIndicator(borderSide: BorderSide(color: Colors.orangeAccent, width: 2.0))
+  ),
+  iconTheme: const IconThemeData(color: darkOrange, shadows: [], size: 28),
+  dividerColor: Colors.white70,
+  dialogTheme: DialogTheme(
+    shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30.0)
+    ),
+  ),
+  colorScheme: ColorScheme.fromSeed(
+      brightness: Brightness.dark,
+      seedColor: Colors.orange
+  ).copyWith(
+      primary: darkOrange,
+      secondary: darkOrange,
+      tertiary: darkBlue,
+      error: Colors.red
+  ),
+  textTheme: const TextTheme().copyWith(
+    bodyLarge: const TextStyle(color: Colors.white),
+    bodyMedium: const TextStyle(color: Colors.white),
+    titleLarge: const TextStyle(color: Colors.white),
+    titleMedium: const TextStyle(color: Colors.white),
+    titleSmall: const TextStyle(color: Colors.white54),
+  ),
+  radioTheme: RadioThemeData(fillColor: MaterialStateProperty.resolveWith((states) => darkOrange)),
+  inputDecorationTheme: const InputDecorationTheme().copyWith(
+      border: OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
+      contentPadding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+      suffixIconColor: lightOrange
+  ),
+  bottomNavigationBarTheme: const BottomNavigationBarThemeData().copyWith(
+      unselectedItemColor: Colors.white54,
+      unselectedLabelStyle: const TextStyle(color: Colors.white54)
+  ),
 );
+
+class LoginRequiredDialog extends StatelessWidget {
+  const LoginRequiredDialog({Key? key, required this.title, required this.content, required this.actions}) : super(key: key);
+  final String title;
+  final String content;
+  final List<Widget> actions;
+
+  Widget dialogContent(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(left: 0.0,right: 0.0),
+      child: Stack(
+        alignment: Alignment.center,
+        clipBehavior: Clip.none,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(
+              top: 18.0,
+            ),
+            margin: const EdgeInsets.only(top: 13.0,right: 8.0),
+            decoration: BoxDecoration(
+                color: Theme.of(context).dialogBackgroundColor,
+                borderRadius: BorderRadius.circular(30.0)
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const SizedBox(height: 20.0),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(title, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18), textAlign: TextAlign.center),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 16.0),
+                  child: Text(content, textAlign: TextAlign.center),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: actions,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Positioned(
+            top: -15.0,
+            child: CircleAvatar(
+              backgroundColor: Theme.of(context).dialogBackgroundColor,
+              radius: 33.0,
+              child: Icon(Icons.info, size: 60, color: Theme.of(context).textTheme.bodyLarge!.color),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+      elevation: 0.0,
+      backgroundColor: Colors.transparent,
+      child: dialogContent(context),
+    );
+  }
+}
